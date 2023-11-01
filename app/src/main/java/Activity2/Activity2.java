0fd428c89640bd19639cc90f8f6ab1a32563f0a1
M4kiseKurisu <21373343@buaa.edu.cn>
2023-11-01 15:18:43 +0800

    init

---
+++ app/src/main/java/Activity2/Activity2.java
@@ -0,0 +1,16 @@
+package Activity2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mainactivity.R;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
---
+++ app/src/main/java/Activity3/Activity3.java
@@ -0,0 +1,16 @@
+package Activity3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mainactivity.R;
+
+public class Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+    }
+}
