d8c625ae3372d2caa1c877fa687fb38a8e7c628c
M4kiseKurisu <21373343@buaa.edu.cn>
2023-11-08 16:39:19 +0800

    主界面

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new MyClickListener());
     }
 
+    private class MyClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (v.equals(button1)) {
+
+            }
+        }
+    }
 }
