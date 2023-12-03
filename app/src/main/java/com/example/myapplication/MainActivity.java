a7114ce5b85139e31c0b1f3ec8f99a48ae020aad
Sske <1097484850@qq.com>
2023-12-03 21:40:37 +0800

    增加登录，可以使用用户id

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.myapplication.tutorial.TutorialActivity;
 
@@ -19,9 +21,13 @@ public class MainActivity extends AppCompatActivity {
     private DataBaseHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPreferences = getSharedPreferences("LoginInfor", MODE_PRIVATE);
+        int userId = sharedPreferences.getInt("UserID", -1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = findViewById(R.id.button1);
+        TextView text = findViewById(R.id.userId);
+        text.setText(String.valueOf(userId));
         button1.setOnClickListener(new MyClickListener());
         dbHelper = new DataBaseHelper(this, "DataBase.db", null, 1);
         dbHelper.getWritableDatabase();
@@ -36,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class MyClickListener implements View.OnClickListener {
+
+
         @Override
         public void onClick(View v) {
             if (v.equals(button1)) {
